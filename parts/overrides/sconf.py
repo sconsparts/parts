4561fa7895ab5b77d2c961c655f614bc00a554e2
dragon512 <dragon512@live.com>
2015-11-09 13:36:27 +0000

    sconf.py edited online with Bit bucket. Update comment with more details

--- parts/overrides/sconf.py
+++ parts/overrides/sconf.py
@@ -3,6 +3,7 @@
 import SCons.SConf
 
 # test to see if we have the function.. else backport it
+# this is needed to support any version pre-Scons 2.4
 try:
     SCons.SConf.NeedConfigHBuilder
 except:
