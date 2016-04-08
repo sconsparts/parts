766543be31c253742d8c056fe6a557c92a286e3e
Jason Kenny <dragon512@live.com>
2016-04-07 22:34:47 -0500

    Fix issue with Intel Compiler not working with Visual studio 15 compiler

--- parts/configurations/default/intelc_win32-any.py
+++ parts/configurations/default/intelc_win32-any.py
@@ -22,10 +22,9 @@ def post_process_func(env):
         ver=float(env['MSVC_VERSION'])
     except:
         raise RuntimeError("You need to define mstools or compatible tool chain with Intel tool chain")
-    for msvc_ver in sorted((12, 11, 10, 9, 8, 7.1, 6), reverse=True):
-        if ver >= msvc_ver:
-            env.AppendUnique(CCFLAGS=['/Qvc{0}'.format(msvc_ver)])
-            break
+    
+    env.AppendUnique(CCFLAGS=['/Qvc{0}'.format(msvc_ver)])
+    
 
     ## code coverage feature additions
     if make_bool(env.get('codecov',False)) == True:
