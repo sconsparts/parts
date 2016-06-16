b6a845b47ca759eda2b9cef4f6823250c659dd5b
Eugene Leskinen <eugene.leskinen@intel.com>
2016-06-16 15:07:36 +0300

    Packaging improvements
    Added test for TarGzFile builder.
    Improved Debian packaging. Instead of post-process logic queue made it use Builder/Scanner technology.
    Adapted dpkg gold-tests.
    
    Gold tests pass

--- parts/pieces/deb_wrapper.py
+++ parts/pieces/deb_wrapper.py
@@ -1,88 +1,106 @@
+'''
+Defines a builder for .deb packages
+'''
+import re
+
+import SCons.Action
+import SCons.Builder
+import SCons.Defaults
+import SCons.Node.FS
+import SCons.Node.Python
 import SCons.Script
+
 import parts.api as api
-import parts.errors
-import parts.common as common
-import parts.glb as glb
-import shutil, os,re
+import parts.packaging as packaging
 
-deb_reg="(\w+)\_([\d.]+)(\-([\w.]+))?\_(\w+)\.deb"
+from parts.errors import GetPartStackFrameInfo, ResetPartStackFrameInfo, SetPartStackFrameInfo
 
-def deb_wrapper_mapper(env, target, sources, **kw):
-    def deb_wrapper():
+DEB_REG = r"(\w+)\_([\d.]+)(\-([\w.]+))?\_(\w+)\.deb"
 
-        #############################################
-        # getting all the sources
-        new_sources, _ = env.Override(kw).GetFilesFromPackageGroups(target, sources)
 
-        #File information required to build source folders and tar files.
-        grps=re.match(deb_reg,target.name,re.IGNORECASE)
+def dep_target_scanner(node, env, _):
+    '''
+    Scanner function for dbpkg builder
+    '''
+    groups = env.MetaTagValue(node, 'groups', 'dpkg', default=[]) or packaging.PackageGroups()
+    files, _ = env.GetFilesFromPackageGroups(
+        node, groups,
+        stackframe=env.MetaTagValue(node, 'stackframe', 'bpkg', default=None))
+    tar_gz_entries = []
+    debian_entries = []
+    pack_dir = node.attributes.pack_dir
+    for entry in files:
+        if (env.MetaTagValue(entry, 'category', 'package') == 'PKGDATA' and
+                'dpkg' in env.MetaTagValue(entry, 'type', 'package', ['dpkg', 'deb', 'DEB'])):
+            new_entry = pack_dir.Dir('debian').Entry(entry.name)
+            if not new_entry.has_builder():
+                env.CCopyAs(new_entry, entry, CCOPY_LOGIC='hard-soft')
+            debian_entries.append(new_entry)
+        else:
+            tar_gz_entries.append(entry)
+    node.attributes.tar_gz = tar_gz = pack_dir.Entry(node.attributes.pack_name + '.tar.gz')
+    if not tar_gz.has_builder():
+        env.TarGzFile(tar_gz, tar_gz_entries, SRC_DIR='$INSTALL_ROOT')
+    node.attributes.files_to_pack = tar_gz_entries
+    return tar_gz_entries + debian_entries + [tar_gz]
 
-        target_name= grps.group(1)
-        target_version= grps.group(2)
-        target_release= grps.group(3)
-        target_arch= grps.group(4)
-
-        src_folder_name = target_name+'-'+target_version
-
-        #############################################
-        ## Sort files in to source group and to control group
-        def filter_func(node):
-            if env.MetaTagValue(node, 'category', 'package')=='PKGDATA':
-                if 'dpkg' in env.MetaTagValue(node, 'types', 'package', ['dpkg','deb','DEB']):
-                    env.CCopy('${{BUILD_DIR}}/_dpkg/{0}/debian'.format(src_folder_name), node)
-                return False
-            return True
-
-        src=filter(filter_func,new_sources)
-
-        pkg_nodes = []
-        for n in src:
-            pk_type=env.MetaTagValue(n, 'category','package')
-            pkg_dir="${{PACKAGE_{0}}}".format(pk_type)
-            pkg_nodes.append(env.Entry('${{BUILD_DIR}}/_dpkg/{0}/{1}/{2}/{3}'.format(src_folder_name,src_folder_name,pkg_dir,env.Dir(n.env['INSTALL_{0}'.format(pk_type)]).rel_path(n))))
-
-        #############################################
-        ##create the source gz file
-        ret = env.CCopyAs(pkg_nodes, src, CCOPY_LOGIC='hard-copy')
-        Tar_Filename = target_name+'_'+target_version
-
-        #############################################
-        ##create the source gz file
-        env.TarGzFile('${{BUILD_DIR}}/_dpkg/{0}.orig.tar.gz'.format(Tar_Filename), src, SRC_DIR="$INSTALL_ROOT")
-        env._dpkg(target,'${{BUILD_DIR}}/_dpkg/{0}'.format(src_folder_name))
-    return deb_wrapper
-
-def dpkg_wrapper(_env, target, sources, **kw):
-    # currently we assume all sources are Group values
-    # will probally change this once we understand better
-    
-    env= _env.Clone(**kw)
-    target = env.arg2nodes(target,env.fs.File)
-    sources= common.make_list(sources)
-    
-    if len(target) > 1:
-        raise SCons.Errors.UserError('Only one target is allowed.')
-
-    if target[0].name.endswith('.deb'):
-        target=[env.Dir("_dpkg").File(target[0])]
-    else:
-        target=[env.Dir("_dpkg").File(target[0].name+".deb")]
-
-    grps=re.match(deb_reg,target[0].name,re.IGNORECASE)
+def dep_emitter(target, source, env):
+    '''
+    Dpkg builder emitter function
+    '''
+    target = env.arg2nodes(target, env.fs.File)
+    grps = re.match(DEB_REG, target[0].name, re.IGNORECASE)
     if grps is None:
-        api.output.error_msg("DEB target files must be in format of <name>_<version>-<release>_<arch>.deb or <name>_<version>_<arch>.deb\n current format of value of target file is '{0}'".format(target[0].name))
+        api.output.error_msg(
+            ("DEB target files must be in format of <name>_<version>-<release>_<arch>.deb "
+             "or <name>_<version>_<arch>.deb\n "
+             "current format of value of target file is '{0}'").format(target[0].name))
+    target_name, target_version, _, _, _ = grps.groups()
+    node = target[0]
+    node.attributes.target_name = target_name
+    node.attributes.pack_name = pack_name = target_name + '_' + target_version
+    node.attributes.pack_dir = env.Dir('${BUILD_DIR}/_dpkg/' + pack_name)
+    source = [env.subst(str(s)) for s in source]
 
-    
-    # subst all source values to get finial package group names
-    sources=[env.subst(s) for s in sources]
+    SetPartStackFrameInfo()
+    env.MetaTag(target, 'dpkg', groups=source, stackframe=GetPartStackFrameInfo())
+    ResetPartStackFrameInfo()
 
-    # delay real work till we have everything loaded
-    glb.engine.add_preprocess_logic_queue(deb_wrapper_mapper(env, target[0], sources, **kw))
-                
-    return target
+    return target, source
 
+def dep_create_install_file(target, source, env):
+    '''
+    Action to create debian/<package>.install file.
+    The file should content file names to be installed and
+    directories where to put them on a target machine
+    '''
+    pack_dir = target[0].attributes.pack_dir
+    install_file = pack_dir.File(
+        'debian/' + target[0].attributes.target_name + '.install')
+    file_name = install_file.abspath
+    install_root = env.Dir('$INSTALL_ROOT')
+    with open(file_name, 'w') as install_file:
+        install_file.write('\n'.join(
+            ' '.join((pack_dir.rel_path(x), 'usr/' + install_root.rel_path(x.dir)))
+            for x in target[0].attributes.files_to_pack))
+    return None
 
-# This is what we want to be setup in parts
-from SCons.Script.SConscript import SConsEnvironment
+api.register.add_builder(
+    'DPKGPackage',
+    SCons.Builder.Builder(
+        action=SCons.Action.Action([
+            SCons.Action.Action(dep_create_install_file),
+            'cd ${TARGET.attributes.pack_dir}; debuild -us -uc -d -b; cd -',
+            SCons.Defaults. Copy('$TARGET', '${TARGET.attributes.pack_dir}/../${TARGET.name}')
+            ], cmdstr='Packing into ${TARGET}'),
+        emitter=dep_emitter,
+        suffix='.deb',
+        ensure_suffix=True,
+        target_scanner=SCons.Script.Scanner(dep_target_scanner),
+        target_factory=SCons.Node.FS.Entry,
+        source_factory=SCons.Node.Python.Value
+    )
+)
 
-SConsEnvironment.DPKGPackage=dpkg_wrapper
+# vim: set et ts=4 sw=4 ai :
+
