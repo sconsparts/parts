e4bee034b23e3254f1ae7bab49fa54923535257d
Jason Kenny <dragon512@live.com>
2017-05-15 15:23:00 +0000

    Merged in dragon512/parts/fix/issue-17 (pull request #23)
    fix for issue 17 - parts does not print version number
    
    Approved-by: Jason Kenny <dragon512@live.com>

--- parts/__init__.py
+++ parts/__init__.py
@@ -1,10 +1,11 @@
-''' 
+'''
 This file is what python loads when the user say import parts
 This file will import main.py and main will do the rest of work needed
 '''
 import sys
 # this tests to see if the script parts is running or if we are loading in SCons.
-if sys._getframe(1).f_code.co_filename.endswith("pkg_resources.py"):
+fname = sys._getframe(1).f_code.co_filename
+if fname.endswith("pkg_resources.py") or fname.endswith('pkg_resources/__init__.py'):
     pass
     #print sys._getframe(1).f_code.co_filename
 else:
