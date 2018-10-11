2764da4302cbb5db8bc74036440c3133c5313ee5
Jason Kenny <dragon512@live.com>
2018-10-11 12:16:45 +0000

    Merged in ccomand (pull request #39)
    Add an improved Command function
    
    * Add an improved Command function
    
    Accepts target and source scanner
    
    Approved-by: Eugene Leskinen

---
+++ parts/pieces/ccommand.py
@@ -0,0 +1,32 @@
+# an enhanced Command function
+# that also accepts target scanners
+import parts.api as api
+
+import SCons.Builder
+import SCons.Scanner.Prog
+# This is what we want to be setup in parts
+from SCons.Script.SConscript import SConsEnvironment
+
+
+def CCommand(self, target, source, action, **kw):
+        """This is basically Command() with target_scanner """
+        bkw = {
+            'action' : action,
+            'target_factory' : self.fs.Entry,
+            'source_factory' : self.fs.Entry,
+        }
+        try: bkw['source_scanner'] = kw['source_scanner']
+        except KeyError: pass
+        else: del kw['source_scanner']
+        try: bkw['target_scanner'] = kw['target_scanner']
+        except KeyError: pass
+        else: del kw['target_scanner']
+        bld = SCons.Builder.Builder(**bkw)
+        return bld(self, target, source, **kw)
+
+
+api.register.add_global_parts_object('ProgramScanner', SCons.Scanner.Prog.ProgramScanner())
+api.register.add_global_object('ProgramScanner', SCons.Scanner.Prog.ProgramScanner())
+
+# adding logic to Scons Enviroment object
+SConsEnvironment.CCommand = CCommand
