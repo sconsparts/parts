5dc6832c473dc9473d7c2da5dd1d298d3471f79e
Phong X Nguyen <p.nguyen@yahooinc.com>
2025-07-23 09:21:55 -0700

    Improve GnuCommon (#32)

--- src/parts/tools/GnuCommon/clang.py
+++ src/parts/tools/GnuCommon/clang.py
@@ -1,5 +1,3 @@
-
-
 import os
 
 from parts.platform_info import SystemPlatform
@@ -7,14 +5,14 @@ from parts.tools.Common.Finders import EnvFinder, PathFinder, ScriptFinder
 from parts.tools.Common.ToolInfo import ToolInfo
 
 from . import android
-from .common import GnuInfo, clang
+from .common import GnuInfo, RedHatToolsetPaths, clang
 
 # mac
 clang.Register(
-    # we assume that the system has the correct libraies installed to do a cross build
+    # we assume that the system has the correct libraries installed to do a cross build
     # or that the user add the extra check for the stuff the need
-    hosts=[SystemPlatform('darwin', 'x86'), SystemPlatform('darwin', 'x86_64'),],
-    targets=[SystemPlatform('darwin', 'x86'), SystemPlatform('darwin', 'x86_64'),],
+    hosts=[SystemPlatform('darwin', 'x86'), SystemPlatform('darwin', 'x86_64'), SystemPlatform('darwin', 'arm64')],
+    targets=[SystemPlatform('darwin', 'x86'), SystemPlatform('darwin', 'x86_64'), SystemPlatform('darwin', 'arm64')],
     info=[
         GnuInfo(
             # standard location, however there might be
@@ -37,33 +35,7 @@ clang.Register(
 )
 
 clang.Register(
-    # we assume that the system has the correct libraies installed to do a cross build
-    # or that the user add the extra check for the stuff the need
-    hosts=[SystemPlatform('darwin', 'arm64')],
-    targets=[SystemPlatform('darwin', 'arm64')],
-    info=[
-        GnuInfo(
-            # standard location, however there might be
-            # some posix offshoot that might tweak this directory
-            # so we allow this to be set
-            install_scanner=[
-                PathFinder(['/usr/bin'])
-            ],
-            opt_dirs=[
-                '/opt/'
-            ],
-            script=None,
-            subst_vars={},
-            shell_vars={'PATH': '${CLANG.INSTALL_ROOT}'},
-            test_file='clang',
-            opt_pattern=r'clang\-?([0-9]+\.[0-9]+\.[0-9]*|[0-9]+\.[0-9]+|[0-9]+)'
-        )
-
-    ]
-)
-
-clang.Register(
-    # we assume that the system has the correct libraies installed to do a cross build
+    # we assume that the system has the correct libraries installed to do a cross build
     # or that the user add the extra check for the stuff the need
     hosts=[SystemPlatform('posix', 'any')],
     targets=[SystemPlatform('posix', 'any')],
@@ -77,13 +49,12 @@ clang.Register(
             ],
             opt_dirs=[
                 '/opt/'
-            ] + ['/opt/rh/llvm-toolset-{0}.0/root/usr/bin/'.format(i) for i in range(5, 20)],
+            ] + RedHatToolsetPaths(r"llvm\-toolset\-(?:[0-9]+\.[0-9]+|[0-9]+)"), # for EL7-style llvm-toolset
             script=None,
             subst_vars={},
             shell_vars={'PATH': '${CLANG.INSTALL_ROOT}'},
             test_file='clang',
             opt_pattern=r'clang\-?([0-9]+\.[0-9]+\.[0-9]*|[0-9]+\.[0-9]+|[0-9]+)'
         )
-
     ]
 )
--- src/parts/tools/GnuCommon/common.py
+++ src/parts/tools/GnuCommon/common.py
@@ -1,17 +1,14 @@
-
-
 import os
 import pprint
 import re
 import subprocess
+from typing import Final, Optional, List
 
 import parts.api as api
 import parts.common
 import SCons.Util
 from parts.tools.Common.ToolInfo import ToolInfo
 from parts.tools.Common.ToolSetting import ToolSetting
-from parts.version import version_range
-
 
 def makeStdBinutilsTool(env, name, compareNames):
     '''
@@ -19,6 +16,7 @@ def makeStdBinutilsTool(env, name, compareNames):
         tool = env.get('BINUTILS', {}).get('AR', env['AR'])
         env['AR'] = parts.tools.Common.toolvar(tool, ['ar'], env)
     '''
+    import parts.tools.Common
     try:
         tool = env.get('BINUTILS', {}).get(name, env[name])
     except KeyError:
@@ -51,22 +49,41 @@ def MatchVersionNumbers(verStr1, verStr2):
 
     return False
 
+# Get RH-style devtoolset paths for use elsewhere. Defaults to returning both EL6-7 devtoolset-<version>
+# and EL8+ gcc-toolset-<version> options.
+def RedHatToolsetPaths(regex: str = r"(gcc\-|dev)toolset\-\d+") -> List[str]:
+    RH_OPT_PATH: Final[str] = "/opt/rh"
+    RH_OPT_ROOT_SUB_PATH: Final[str] = "root/usr/bin"
+
+    pattern = re.compile(regex)
+
+    if os.path.exists("/opt/rh"):
+        return [ os.path.join(RH_OPT_PATH, d, RH_OPT_ROOT_SUB_PATH) for d in os.listdir(RH_OPT_PATH) if pattern.fullmatch(d)]
+    else:
+        return []
 
 class GnuInfo(ToolInfo):
+    DEFAULT_TEST_FILE_SUFFIX_PATTERN: Final[str] = r'\-?([0-9]+\.[0-9]+\.[0-9]*|[0-9]+\.[0-9]+|[0-9]+)'
+    DEFAULT_VERSION_PATTERN: Final[str] = r'[vV ]([0-9]+\.[0-9]+\.[0-9]*|[0-9]+\.[0-9]+)'
 
-    def __init__(self, install_scanner, opt_dirs, script, subst_vars, shell_vars, test_file, opt_pattern=None):
+    def __init__(self, install_scanner, opt_dirs, script, subst_vars, shell_vars, test_file,
+                 opt_pattern: Optional[str]=None,
+                 test_file_prefix_pattern: Optional[str]=None, test_file_suffix_pattern: Optional[str]=DEFAULT_TEST_FILE_SUFFIX_PATTERN,
+                 version_pattern: str=DEFAULT_VERSION_PATTERN):
         ToolInfo.__init__(self, "*", install_scanner, script, subst_vars, shell_vars, test_file)
         self.opt_paths = parts.common.make_list(opt_dirs)
-        self.found = None
-        self.opt_pattern = opt_pattern
+        self.found: Optional[dict] = None
+        self.opt_pattern: Optional[str] = opt_pattern
+        self.test_file_prefix_pattern: str = test_file_prefix_pattern or ""
+        self.test_file_suffix_pattern: str = test_file_suffix_pattern or ""
+        self.version_pattern: str = version_pattern
 
-    def get_default_ver(self, path):
+    def get_default_ver(self, path) -> Optional[str]:
         fullpath = path
         if os.path.isfile(fullpath):
             # this is to get the version number
             line = subprocess.check_output([fullpath, '--version'], stderr=subprocess.STDOUT)
-            match = re.search(r'[vV ]([0-9]+\.[0-9]+\.[0-9]*|[0-9]+\.[0-9]+)', line.decode())
-            if match:
+            if match := re.search(self.version_pattern, line.decode()):
                 line = match.group(1)
                 return line
 
@@ -86,10 +103,10 @@ class GnuInfo(ToolInfo):
         return self.version
 
     def get_root(self, version):
-        if self.found is None:
+        if not self.found:
             root = ToolInfo.get_root(self, None)
             self.scan_query(root)
-        if self.found is not None:
+        if self.found:
             try:
                 k = sorted(self.found.keys())
                 k.reverse()
@@ -106,7 +123,7 @@ class GnuInfo(ToolInfo):
     def get_shell_env(self, env, namespace, version, install_root, script, tool=None):
         # call base logic for exists
         ret = ToolInfo.get_shell_env(self, env, namespace, version, install_root, script, tool)
-        if ret is not None and self.found is not None:
+        if ret and self.found:
             # get correct tool value in namespace
             k = sorted(self.found.keys())
             k.reverse()
@@ -123,7 +140,7 @@ class GnuInfo(ToolInfo):
         # this logic is different as we have a built in scanner
 
         # If we have a root path passed in use it
-        if root_path is not None:
+        if root_path:
             found = self.scan_query(root_path, False)
         elif SCons.Util.is_String(use_script):
             found = {'0.0.0': (None, None)}
@@ -132,83 +149,75 @@ class GnuInfo(ToolInfo):
             root = ToolInfo.get_root(self, None)
             found = self.scan_query(root)
 
-        if found is None:
+        if not found:
             return None
 
-        ret = {}
+        ret: dict = {}
         for v, p in found.items():
-            # print "testing",v,p[0],p[1]
-            tmp = self.exists(env, namespace, v, p[0], use_script, p[1])
-            if tmp is not None:
+            if tmp := self.exists(env, namespace, v, p[0], use_script, p[1]):
                 ret.update(self.make_ver_shell_env_set(v, tmp))
 
         return ret
 
-    def scan_query(self, install_root, opt_scan=True):
-        api.output.verbose_msgf(['gnu_info', "tool_info"], "query scan install_root={root}", root=install_root)
-        ret = {}
-        if self.found is None:
-            reg = re.compile(self.test_file.replace('+', r'\+') + r'\-?([0-9]+\.[0-9]+\.[0-9]*|[0-9]+\.[0-9]+|[0-9]+)', re.I)
-            if opt_scan == True:
-                if self.opt_pattern is not None:
-                    opt_reg = re.compile(self.opt_pattern, re.I)
-                else:
-                    opt_reg = reg
+    def scan_query(self, install_root: Optional[str], opt_scan: bool=True):
+        found: dict = {}
+
+        def set_found_tuple(path, item):
+            nonlocal found
+            pathwtool = os.path.join(path, item)
+            if version := self.get_default_ver(pathwtool):
+                api.output._trace_msgf(['gnu_info', "tool_info"], f"query scan found[{version}] = ({path}, {pathwtool})")
+                found[version] = (path, pathwtool)
+
+        if not self.found:
+            test_file_pattern = re.compile(self.test_file_prefix_pattern + self.test_file.replace('+', r'\+') + self.test_file_suffix_pattern, re.IGNORECASE)
+            if opt_scan:
+                opt_dir_pattern = test_file_pattern if not self.opt_pattern else re.compile(self.opt_pattern, re.IGNORECASE)
                 # see if we can find a version in the optional directories provided
-                # based on pattern of dir/tool-ver/bin/tool
+                # based on pattern of either:
+                #   * dir/tool
+                #   * dir/tool-ver/bin/tool
                 # this can be /opt or any other user provided directory
                 for d in self.opt_paths:
                     # check that is exists and is directory
                     if os.path.isdir(d):
-                        # look for all directories here that match pattern
+                        api.output.verbose_msgf(['gnu_info', "tool_info"], f"query scan opt_root={d}")
                         for item in os.listdir(d):
-                            result = opt_reg.match(item)
-                            if result:
+                            # look for file match
+                            if self.test_file == item or test_file_pattern.fullmatch(item):
+                                set_found_tuple(d, item)
+                            # look for all directories here that match pattern
+                            if opt_dir_pattern.match(item):
                                 fullpath = os.path.join(d, item, 'bin')
                                 if not os.path.exists(fullpath):
                                     continue
+                                api.output.verbose_msgf(['gnu_info', "tool_info"], f"query scan opt_root={fullpath}")
                                 for fitem in os.listdir(fullpath):
-                                    result = reg.match(fitem)
-                                    if result or self.test_file == fitem:
-                                        pathwtool = os.path.join(fullpath, fitem)
-                                        tmp = self.get_default_ver(pathwtool)
-                                        if tmp is not None:
-                                            ret[tmp] = (fullpath, pathwtool)
-                for d in self.opt_paths:
-                    if os.path.isdir(d):
-                        for fitem in os.listdir(d):
-                            result = reg.match(fitem)
-                            if result or self.test_file == fitem:
-                                pathwtool = os.path.join(d, fitem)
-                                tmp = self.get_default_ver(pathwtool)
-                                if tmp is not None:
-                                    ret[tmp] = (d, pathwtool)
+                                    if self.test_file == fitem or test_file_pattern.fullmatch(fitem):
+                                        set_found_tuple(fullpath, fitem)
 
-            if install_root is not None:
+            if install_root:
+                api.output.verbose_msgf(['gnu_info', "tool_info"], f"query scan install_root={install_root}")
+
                 # see if in this area we have any possible matches of different versions
-                # using a convension of tool-ver ( ie gcc-3.4 or gcc-3 )
-                # overrides anything in /opt/...
+                # using a convention of [prefix]-tool-[suffix]. By default we look for
+                # tool and tool-suffix. For example, `gcc`, `gcc-14`, `clang-15.2`
+                # overrides anything discovered in opt_scan
                 for item in os.listdir(install_root):
-                    result = reg.match(item)
-                    if result:
-                        fullpath = os.path.join(install_root, item)
-                        tmp = self.get_default_ver(fullpath)
-                        if tmp is not None:
-                            ret[tmp] = (install_root, fullpath)
+                    if self.test_file == item or test_file_pattern.fullmatch(item):
+                        set_found_tuple(install_root, item)
 
                 # see if we can find a default version in the directories provided
                 # overrides any previous finds of same version
+                api.output.trace_msgf(['gnu_info', "tool_info"], f"query scan install_root priority override")
+                set_found_tuple(install_root, self.test_file)
 
-                tmp = self.get_default_ver(os.path.join(install_root, self.test_file))
-                if tmp is not None:
-                    ret[tmp] = (install_root, os.path.join(install_root, self.test_file))
+            self.found = found
 
-                self.found = ret
-
-        if self.found == {}:
+        if not self.found:
             api.output.verbose_msgf(['gnu_info', "tool_info"], "Nothing was found!")
             return None
-        api.output.verbose_msgf(['gnu_info', "tool_info"], f"Found:{pprint.pformat(ret)}")
+        api.output.verbose_msgf(['gnu_info', "tool_info"], f"Found:\n{pprint.pformat(found)}")
         return self.found
 
 
